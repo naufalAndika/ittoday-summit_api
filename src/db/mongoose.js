2bac2b1a2e57c7127285dad1e2a996c57ae00aae
Naufal Andika <andikaa.naufal@gmail.com>
2019-08-28 21:21:40 +0700

    configure for upload to heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/summit-api', {
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/summit-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: true
